b7ec974c5133249e60c0ed0a9c1f11e50ed7e3e5
charm <ichchinmay.in@gmail.com>
2024-08-24 22:47:05 +0530

    vite updated

--- apple-project/vite.config.js
+++ apple-project/vite.config.js
@@ -1,15 +1,7 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "jsm-m3",
-    project: "javascript-react"
-  })],
-
-  build: {
-    sourcemap: true
-  }
+  plugins: [react()],
 })
